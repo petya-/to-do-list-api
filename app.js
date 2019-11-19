9bb1e1a46953c2231333519f14cc00354be97d7a
Petya Buchkova <petya.buchkova@gmail.com>
2019-11-19 12:26:35 +0100

    Removed templating engine and public folder

--- app.js
+++ app.js
@@ -9,15 +9,9 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false,
-}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
